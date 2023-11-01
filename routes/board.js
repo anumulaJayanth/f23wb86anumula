72709f70f936987017102401a543ae642f1b516c
Jayanth Anumula <52216969+anumulaJayanth@users.noreply.github.com>
2023-11-01 13:39:17 -0500

    choose

--- routes/board.js
+++ routes/board.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Define the calculateCellValue function
+function calculateCellValue(row, col) {
+  if (row === col) {
+    return 1;
+  } else if (row === col + 1) {
+    return 2;
+  } else if (row === col - 1) {
+    return row-1;
+  } else {
+    return 0;
+  }
+}
+
 router.get('/', (req, res) => {
-  let query = req.query;
+  const query = req.query;
   console.log(`rows: ${query.rows}`);
   console.log(`cols: ${query.cols}`);
-  res.render('board', { title: 'Board Display', query: query });
+  const rows = Number(query.rows);
+  const cols = Number(query.cols);
+
+  if (
+    Number.isNaN(rows) ||
+    Number.isNaN(cols) ||
+    rows < 1 || rows > 10 ||
+    cols < 1 || cols > 10
+  ) {
+    res.render('board', { title: 'Board Display', query: null, calculateCellValue });
+  } else {
+    res.render('board', { title: 'Board Display', query, calculateCellValue });
+  }
 });
 
-
-module.exports = router;
+module.exports = router;
--- routes/choose.js
+++ routes/choose.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fruits', { title: 'Search Results - fruits' });
+  res.render('choose', { title: 'Choose' });
 });
 
 module.exports = router;
